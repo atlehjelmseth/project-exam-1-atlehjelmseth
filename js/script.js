3b6e3ce1b94cb9976b4bf87b5b95aa69c55beb95
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2022-11-22 17:46:01 +0100

    Update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const url = "https://exam-one.eltprod.no/wp-json/wp/v2/posts";
+
+const posts = document.querySelector(".carousel");
+
+async function JacketsApiFunction() {
+  try{const response = await fetch(url);
+    const results = await response.json();
+
+    posts.innerHTML = "";
+    console.log(results)
+    for(let i = 0; i < results.length; i++) {
+      posts.innerHTML += `<div class="import-posts">
+                                <img src="${results[i].content.rendered}">`;
+
+    }
+  }catch (error) {
+    console.log("error")
+  }
+
+}
+
+JacketsApiFunction()
