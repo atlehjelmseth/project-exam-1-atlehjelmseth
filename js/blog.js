b8f63537ffc80ec2f2b12408c47a5d666b58608d
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2022-11-28 14:16:55 +0100

    Update

---
+++ js/blog.js
@@ -0,0 +1,42 @@
+const url = "https://exam-one.eltprod.no/wp-json/wp/v2/posts?_embed";
+const posts = document.querySelector(".blog");
+
+
+async function blogApi() {
+  try{const response = await fetch(url);
+    const results = await response.json();
+
+    posts.innerHTML = "";
+    
+    
+    for(let i = 0; i < results.length; i++) {
+      if (i === 9) { break; }
+      const pictures = results[i]._embedded['wp:featuredmedia'][0].source_url;
+      const title = results[i].title.rendered;
+      const paragraph = results[i].excerpt.rendered;
+      const id = results[i].id;
+
+
+
+      posts.innerHTML += `
+                            <div class="blog-container">
+                             <div class="product-image">
+                             <img src="${pictures}" class="product-thumb" alt="fortum logo">
+                            </div>
+                            <p class="blog-title">${title}</p>
+                            <p>${paragraph}</p>
+                            <a href="blogpost.html?id=${id}" class="button_blog">Read more</a>
+                          </div>
+                                  `;
+                                  
+    }
+
+  }catch (error) {
+    posts.innerHTML = "error";
+    console.log("error")
+  }
+
+}
+
+
+blogApi()
---
+++ js/details.js
@@ -0,0 +1,46 @@
+const urlDetails = "https://exam-one.eltprod.no/wp-json/wp/v2/posts";
+
+const spesifications = document.querySelector(".details");
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+console.log(id);
+
+
+const specs = urlDetails + "/" + id + "?_embed";
+
+console.log(specs);
+
+async function jacketSpecs() {
+  try {const response = await fetch(specs);
+       const resultsSpec = await response.json();
+
+      console.log(resultsSpec)
+      const pictures = resultsSpec._embedded['wp:featuredmedia'][0].source_url;
+      const title = resultsSpec.title.rendered;
+      const paragraph = resultsSpec.excerpt.rendered;
+      const id = resultsSpec.id;
+
+      spesifications.innerHTML = "";
+
+      spesifications.innerHTML += `<div class="details-container">
+      <p class="blog-title">${title}</p>
+      <div class="product-image">
+      <img src="${pictures}" class="product-thumb" alt="fortum logo">
+     </div>
+
+     <p>${paragraph}</p>
+   </div>
+                                  `;
+
+
+      }catch (error) {
+        spesifications.innerHTML = "Unable to connect to the API";
+    }
+   }
+
+   jacketSpecs()
+
+   
