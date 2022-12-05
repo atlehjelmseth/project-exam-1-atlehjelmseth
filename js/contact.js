855a9c2f14933f915db52d39423d2db0af4a01e4
atlehjelmseth <atle.hjelmseth@stud.noroff.no>
2022-12-05 12:35:23 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -41,25 +41,35 @@ function validateForm() {
     success.style.display = "block";
     button.style.display = "none";
 
-  }
-  
+  } 
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-
+  
     const formElement = event.target,
       { action, method } = formElement,
       body = new FormData(formElement);
-
-  fetch(action, {
-    method,
-    body
-  })
-
-};
-
-const formElement = document.querySelector("form");
-
-formElement.addEventListener("submit", formSubmissionHandler);
+  
+    fetch(action, {
+      method,
+      body
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        // Determine if the submission is not valid
+        if (isFormSubmissionError(response)) {
+          // Handle the case when there are validation errors
+        }
+        // Handle the happy path
+      })
+      .catch((error) => {
+        // Handle the case when there's a problem with the request
+      });
+  };
+  
+  const formElement = document.querySelector("form");
+  
+  formElement.addEventListener("submit", formSubmissionHandler);
 
 
 }
